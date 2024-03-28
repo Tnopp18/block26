39ef51ca1a40d0adc4e2ac894c7588e849944a5e
Thomas Noppenberger <thomas.lee.noppenberger@gmail.com>
2024-03-28 11:33:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactList from './components/contactlist';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/contactlist.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import ContactRow from "./contactrow";
+
+export default function ContactList() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
+
+  console.log("Contacts: ", contacts);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {contacts.map((contact) => {
+          return <ContactRow key={contact.id} contact={contact} />;
+        })}
+      </tbody>
+    </table>
+  );
+}
